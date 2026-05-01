53e317e04e63f4794519451b976b359705b45a6e
Abdeslam El Ftouh <abdeslamelftouh@gmail.com>
2026-05-01 13:21:05 +0200

    ADD main + main.style

---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+import styles from './Main.module.css';
+function Main() {
+    return (
+        <main className={styles.mainContainer}>
+            <h3>--&gt; Content goes here &lt;--</h3>
+        </main>
+    )
+}
+
+export default Main;
