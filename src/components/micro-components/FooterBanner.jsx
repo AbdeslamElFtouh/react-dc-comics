1aac5103267dce0a7de6483ca78d53d41707b9bf
Abdeslam El Ftouh <abdeslamelftouh@gmail.com>
2026-05-03 14:57:13 +0200

    ADD FooterBanner 1. creazione codice dinamico con MAP e render function 2. migliorie dello stile 3. creazione file data del FooterBanner

--- src/components/micro-components/FooterBanner.jsx
+++ src/components/micro-components/FooterBanner.jsx
@@ -1,33 +1,26 @@
 import style from './FooterBanner.module.css';
+import { lista_banner_items } from '../../data/footer_banner.js';
+
+function render_banner_items(items) {
+    return (items.map((item) => {
+        const { src, text, id } = item;
+        return (
+            <div key={id} className={style.footerBannerItem}>
+                <img src={src} alt={text.toLowerCase()} className={style.footerBannerIcon} />
+                <span>{text}</span>
+            </div>
+        );
+    }))
+};
 
 function FooterBanner() {
+
+    let banner_items = render_banner_items(lista_banner_items);
+
     return (
         <section className={style.footerBanner}>
             <div className={style.footerBannerContainer}>
-                <div className={style.footerBannerItem}>
-                    <img src="/img/buy-comics-digital-comics.png" alt="Digital Comics" className={style.footerBannerIcon}/>
-                    <span>DIGITAL COMICS</span>
-                </div>
-
-                <div className={style.footerBannerItem}>
-                    <img src="/img/buy-comics-merchandise.png" alt="DC Merchandise" className={style.footerBannerIcon}/>
-                    <span>DC MERCHANDISE</span>
-                </div>
-
-                <div className={style.footerBannerItem}>
-                    <img src="/img/buy-comics-subscriptions.png" alt="Subscription" className={style.footerBannerIcon}/>
-                    <span>SUBSCRIPTION</span>
-                </div>
-
-                <div className={style.footerBannerItem}>
-                    <img src="/img/buy-comics-shop-locator.png" alt="Comic Shop Locator" className={style.footerBannerIcon}/>
-                    <span>COMIC SHOP LOCATOR</span>
-                </div>
-
-                <div className={style.footerBannerItem}>
-                    <img src="/img/buy-dc-power-visa.svg" alt="DC Power Visa" className={style.footerBannerIcon}/>
-                    <span>DC POWER VISA</span>
-                </div>
+                {banner_items}
             </div>
         </section>
     )
---
+++ src/data/footer_banner.js
@@ -0,0 +1,31 @@
+const lista_banner_items = [
+    {
+        id: 1,
+        src: "/img/buy-comics-digital-comics.png",
+        text: "DIGITAL COMICS"
+    },
+    {
+        id: 2,
+        src: "/img/buy-comics-merchandise.png",
+        text: "DC MERCHANDISE"
+    },
+    {
+        id: 3,
+        src: "/img/buy-comics-subscriptions.png",
+        text: "SUBSCRIPTION"
+    },
+    {
+        id: 4,
+        src: "/img/buy-comics-shop-locator.png",
+        text: "COMIC SHOP LOCATOR"
+    },
+    {
+        id: 5,
+        src: "/img/buy-dc-power-visa.svg",
+        text: "DC POWER VISA"
+    }
+];
+
+export {
+    lista_banner_items
+};
